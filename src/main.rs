021d93961e55a60bb8d94daacfc049535709ac37
Daniel <daniel.hub@outlook.de>
2022-03-31 11:09:25 +0000

    Add function that fetches the result website

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     dotenv()?;
 
     let client = ClientBuilder::new().cookie_store(true).build()?;
-    login(&client)?;
+    let auth_arguments = login(&client)?;
+    let results = fetch_results(&client, &auth_arguments)?;
+
+    println!("{}", results);
 
     Ok(())
 }
 
-fn login(client: &Client) -> Result<(), Box<dyn std::error::Error>> {
-    let login_url = format!("{}/scripts/mgrqispi.dll", BASE_URL);
+fn login(client: &Client) -> Result<String, Box<dyn std::error::Error>> {
+    let url = format!("{}/scripts/mgrqispi.dll", BASE_URL);
 
     let username = std::env::var("DUALIS_EMAIL")?;
     let password = std::env::var("DUALIS_PASSWORD")?;
-    let login_form: &[(&str, &str)] = &[
+    let form_data: &[(&str, &str)] = &[
         ("usrname", &username),
         ("pass", &password),
         ("APPNAME", "CampusNet"),
@@ -33,12 +36,15 @@ fn login(client: &Client) -> Result<(), Box<dyn std::error::Error>> {
         ("platform", ""),
     ];
 
-    let response = client.post(login_url).form(login_form).send()?;
+    let response = client.post(url).form(form_data).send()?;
 
     // Response code should always be 200. If the response body is too large,
     // it usually means that the login failed because a html page with a error is returned.
     let status = response.status();
-    if !status.is_success() || response.text()?.len() > 500 {
+    let refresh_header = response.headers().get("REFRESH").unwrap().to_str()?.to_string();
+    let content = response.text()?;
+
+    if !status.is_success() || content.len() > 500 {
         return Err(format!(
             "Login failed. Please check your credentials. Status code: {}",
             status
@@ -48,5 +54,20 @@ fn login(client: &Client) -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Login successful!");
 
-    Ok(())
+    // TODO: unuglify this constant substring
+    Ok(refresh_header[84..].to_string())
+}
+
+fn fetch_results(client: &Client, auth_arguments: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let url = format!("{}/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=STUDENT_RESULT&ARGUMENTS={}", BASE_URL, auth_arguments);
+
+    let response = client.get(url).send()?;
+    let status = response.status();
+    let content = response.text()?;
+
+    if !status.is_success() || content.len() < 500 {
+        return Err("Failed to fetch results.".into())
+    }
+
+    Ok(content)
 }
