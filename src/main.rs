a2e056c0ca8e77bbbbb3cfec6ea23f0f543bc3c5
Daniel <daniel.hub@outlook.de>
2022-03-31 11:58:46 +0000

    Add document parsing of course results

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use dotenv::dotenv;
+use regex::Regex;
 use reqwest::blocking::{Client, ClientBuilder};
+use scraper::{Html, Selector, ElementRef};
 
 const BASE_URL: &str = "https://dualis.dhbw.de";
 
@@ -8,14 +10,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let client = ClientBuilder::new().cookie_store(true).build()?;
     let auth_arguments = login(&client)?;
-    let results = fetch_results(&client, &auth_arguments)?;
+    let result_html = fetch_results(&client, &auth_arguments)?;
+    let results = parse_results(&result_html);
 
-    println!("{}", results);
+    for result in results {
+        println!("Id: {}, Name: {}, scored: {}", result.course_id, result.course_name, result.scored);
+    }
 
     Ok(())
 }
 
 fn login(client: &Client) -> Result<String, Box<dyn std::error::Error>> {
+    println!("Logging in...");
     let url = format!("{}/scripts/mgrqispi.dll", BASE_URL);
 
     let username = std::env::var("DUALIS_EMAIL")?;
@@ -41,7 +47,12 @@ fn login(client: &Client) -> Result<String, Box<dyn std::error::Error>> {
     // Response code should always be 200. If the response body is too large,
     // it usually means that the login failed because a html page with a error is returned.
     let status = response.status();
-    let refresh_header = response.headers().get("REFRESH").unwrap().to_str()?.to_string();
+    let refresh_header = response
+        .headers()
+        .get("REFRESH")
+        .unwrap()
+        .to_str()?
+        .to_string();
     let content = response.text()?;
 
     if !status.is_success() || content.len() > 500 {
@@ -58,16 +69,89 @@ fn login(client: &Client) -> Result<String, Box<dyn std::error::Error>> {
     Ok(refresh_header[84..].to_string())
 }
 
-fn fetch_results(client: &Client, auth_arguments: &str) -> Result<String, Box<dyn std::error::Error>> {
-    let url = format!("{}/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=STUDENT_RESULT&ARGUMENTS={}", BASE_URL, auth_arguments);
+fn fetch_results(
+    client: &Client,
+    auth_arguments: &str,
+) -> Result<String, Box<dyn std::error::Error>> {
+    println!("Fetching results...");
+    let url = format!(
+        "{}/scripts/mgrqispi.dll?APPNAME=CampusNet&PRGNAME=STUDENT_RESULT&ARGUMENTS={}",
+        BASE_URL, auth_arguments
+    );
 
     let response = client.get(url).send()?;
     let status = response.status();
     let content = response.text()?;
 
     if !status.is_success() || content.len() < 500 {
-        return Err("Failed to fetch results.".into())
+        return Err("Failed to fetch results.".into());
     }
+
+    println!("Successfully fetched results!");
 
     Ok(content)
 }
+
+#[derive(Debug)]
+struct CourseResult {
+    course_id: String,
+    course_name: String,
+    scored: bool,
+}
+
+impl CourseResult {
+    fn new(course_id: String, course_name: String, scored: bool) -> Self {
+        Self {
+            course_id,
+            course_name,
+            scored,
+        }
+    }
+}
+
+fn parse_results(result_html: &str) -> Vec<CourseResult> {
+    println!("Parsing results...");
+    let document = Html::parse_document(result_html);
+    println!("Successfully Parsed results.");
+
+    let mut results = Vec::new();
+    let course_name_replace_regex = Regex::new("<!--.+-->").unwrap();
+    
+    let table_rows_selector = Selector::parse("tbody tr").unwrap();
+    let table_rows = document.select(&table_rows_selector);
+    for row in table_rows {
+        // Filter useless rows
+        let row_classes = row.value().attr("class").unwrap_or_default();
+        if row_classes.contains("subhead") || row_classes.contains("level00") {
+            continue;
+        }
+
+        let cell_selector = Selector::parse("td").unwrap();
+        let cells: Vec<ElementRef> = row.select(&cell_selector).collect();
+
+        if cells.len() < 6 {
+            continue;
+        }
+
+        if cells.iter().any(|cell| !cell.value().attr("class").unwrap_or_default().contains("tbdata")) {
+            continue;
+        }
+
+        // Initial parsing:
+        let course_id: String = cells[0].text().collect();
+        let course_name: String = cells[1].text().map(|text_part| text_part.trim()).collect();
+        let scored = cells[5].value().attr("title").unwrap_or("offen").to_lowercase() != "offen";
+
+        // Value fixing:
+        // Dualis is so bad that they use xml/html comments inside a script tag LMAO
+        // Replace line endings so everything is a single line for the regex.
+        let course_name = course_name.replace('\n', "");
+        let course_name= course_name_replace_regex.replace_all(&course_name, "").to_string();
+        
+        let course_result = CourseResult::new(course_id, course_name, scored);
+        results.push(course_result);
+    }
+
+    results
+}
+
